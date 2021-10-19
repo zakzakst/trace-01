78afa153813b27b6167cc5de5ed6a6ccfc9e339e
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-10-19 22:22:56 +0900

    トップへ戻る作成

--- src/js/modules/go-top.js
+++ src/js/modules/go-top.js
@@ -1,17 +1,33 @@
 import $ from 'jquery';
 
-export function goTop() {
-  const el = $('#js-go-top');
-  if (!el) {
-    return;
-  }
-  addEventGoTop();
+export function GoTopInit() {
+  const goTop = new GoTop();
+  goTop.init();
+}
 
-  function addEventGoTop() {
-    const speed = 600;
-    el.find('a').on('click', (e) => {
-      e.preventDefault();
-      $('body,html').animate({ scrollTop: 0 }, speed);
-    });
+class GoTop {
+  constructor() {
+    this.$el = $('#js-go-top');
+  }
+
+  // 初期化
+  init() {
+    console.log('トップへ戻る初期化');
   }
 }
+
+// export function GoTop() {
+//   const el = $('#js-go-top');
+//   if (!el) {
+//     return;
+//   }
+//   addEventGoTop();
+
+//   function addEventGoTop() {
+//     const speed = 600;
+//     el.find('a').on('click', (e) => {
+//       e.preventDefault();
+//       $('body,html').animate({ scrollTop: 0 }, speed);
+//     });
+//   }
+// }
