cdca3af6be087c903aa17f6cae8f93b085fbfd03
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-10-24 03:49:42 +0900

    ブログリスト作成

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,12 @@ import { HeaderInit } from './modules/header';
 import { FooterGalleryInit } from './modules/footer-gallery';
 import { GoTopInit } from './modules/go-top';
 import { PageLoaderInit } from './modules/page-loader';
+import { BlogCardInit } from './modules/blog-card';
 
 $(function () {
   HeaderInit();
   FooterGalleryInit();
   GoTopInit();
   PageLoaderInit();
+  BlogCardInit();
 });
